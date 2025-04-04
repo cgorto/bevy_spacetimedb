b682ec37541541afd31b4011160c8f0cfdf1ea94
Julien Lavocat <julien.lavocat@gmail.com>
2025-04-04 11:33:39 +0200

    fix: add remaining events and use correct type for reducer event

--- src/aliases.rs
+++ src/aliases.rs
@@ -1,7 +1,9 @@
 use bevy::prelude::EventReader;
-use spacetimedb_sdk::ReducerEvent;
 
-use crate::{DeleteEvent, InsertEvent, InsertUpdateEvent, UpdateEvent};
+use crate::{
+    DeleteEvent, InsertEvent, InsertUpdateEvent, ReducerResultEvent, StdbConnectedEvent,
+    StdbConnectionErrorEvent, StdbDisconnectedEvent, UpdateEvent,
+};
 
 /// A type alias for a Bevy event reader for InsertEvent<T>.
 pub type ReadInsertEvent<'w, 's, T> = EventReader<'w, 's, InsertEvent<T>>;
@@ -15,5 +17,14 @@ pub type ReadDeleteEvent<'w, 's, T> = EventReader<'w, 's, DeleteEvent<T>>;
 /// A type alias for a Bevy event reader for InsertUpdateEvent<T>.
 pub type ReadInsertUpdateEvent<'w, 's, T> = EventReader<'w, 's, InsertUpdateEvent<T>>;
 
-/// A type alias for a Bevy event reader for ReducerEvent<T>.
-pub type ReadReducerEvent<'w, 's, T> = EventReader<'w, 's, ReducerEvent<T>>;
+/// A type alias for a Bevy event reader for ReducerResultEvent<T>.
+pub type ReadReducerEvent<'w, 's, T> = EventReader<'w, 's, ReducerResultEvent<T>>;
+
+/// A type alias for a Bevy event reader for StdbConnectedEvent.
+pub type ReadStdbConnectedEvent<'w, 's> = EventReader<'w, 's, StdbConnectedEvent>;
+
+/// A type alias for a Bevy event reader for StdbDisconnectedEvent.
+pub type ReadStdbDisconnectedEvent<'w, 's> = EventReader<'w, 's, StdbDisconnectedEvent>;
+
+/// A type alias for a Bevy event reader for StdbConnectionErrorEvent.
+pub type ReadStdbConnectionErrorEvent<'w, 's> = EventReader<'w, 's, StdbConnectionErrorEvent>;
